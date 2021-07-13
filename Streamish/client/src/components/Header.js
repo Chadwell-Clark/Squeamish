49773fda17fbcdf5ef05e23377e66a2df73f1824
Chadwell Clark <wcclark23@comcast.net>
2021-07-12 20:14:14 -0500

    More styling

--- Streamish/client/src/components/Header.js
+++ Streamish/client/src/components/Header.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="navbar navbar-expand navbar-dark bg-info">
-      <Link to="/" className="navbar-brand ms-4">
+      <Link to="/" className="navbar-brand ms-4 fs-3">
         StreamISH
       </Link>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <Link to="/" className="nav-link">
+          <Link to="/" className="nav-link fs-4">
             Feed
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/videos/add" className="nav-link">
+          <Link to="/videos/add" className="nav-link fs-4">
             New Video
           </Link>
         </li>
